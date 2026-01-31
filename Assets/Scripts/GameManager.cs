ffc92ddfbf16fd7111a3edc6293a42e47dc15db5
blade-x7 <portia.flaherty@icloud.com>
2026-01-31 17:22:01 -0500

    audio pitchers for actors

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] Image timerDisplay;
     float timerFillPercent;
 
+    public AudioSource source;
+    public AudioPitcherSO testSounds;
+
     void Start()
     {
         if (instance == null)
@@ -39,5 +42,11 @@ public class GameManager : MonoBehaviour
 
         timerFillPercent = currTime / roundDuration;
         timerDisplay.fillAmount = timerFillPercent;
+    }
+
+    [ContextMenu("Test Audio")]
+    public void TestAudio()
+    {
+        testSounds.Play(source);
     }
 }
