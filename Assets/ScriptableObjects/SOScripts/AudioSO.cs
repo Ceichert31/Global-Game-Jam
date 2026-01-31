9ee5f8b67d59722544680a71c36b0e6a01c11096
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-01-31 11:46:18 -0500

    chore: Downgraded to unity 2022

--- Assets/ScriptableObjects/SOScripts/AudioSO.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public abstract class AudioSO : ScriptableObject
-{
-    public abstract void Play(AudioSource source);
-}
