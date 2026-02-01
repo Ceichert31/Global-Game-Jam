23e3464e463b5e90b925458b8853e563d15968fc
cocoatehcat <cocoa.teh.cat@gmail.com>
2026-02-01 10:23:04 -0500

    feat: animated one character and created random sound script

---
+++ Assets/Scripts/PickRanSound.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickRanSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource source;
+    [SerializeField]
+    private List<AudioClip> clipList;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time + 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < Time.time)
+        {
+            AudioClip clip = PickSound();
+            source.PlayOneShot(clip);
+            timer = clip.length;
+        }
+        
+    }
+
+    // Pick a random sound
+    private AudioClip PickSound()
+    {
+        int random = Random.Range(0, clipList.Count);
+
+        return clipList[random];
+    }
+}
